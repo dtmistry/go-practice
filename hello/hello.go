0050f5b24d8436774d4242346faf29b1a33348f9
dtmistry <mistrydee@gmail.com>
2014-02-20 12:14:48 -0600

    Added newmath lib

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-practice/newmath"
+)
 
 func main() {
-	fmt.Printf("Hello, World!!\n")
+	fmt.Printf("Hello, World!! Sqrt(9) = %v\n", newmath.Sqrt(9))
 }
